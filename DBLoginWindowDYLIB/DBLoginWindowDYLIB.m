42dc3523fe16db8433eb4a275b28324e156f571e
w0lfschild <aguywithlonghair@yahoo.com>
2018-06-07 21:02:00 -0700

    6.1.0

--- DBLoginWindowDYLIB/DBLoginWindowDYLIB.m
+++ DBLoginWindowDYLIB/DBLoginWindowDYLIB.m
@@ -7,7 +7,6 @@
 
 @import AppKit;
 #import "FConvenience.h"
-#import "DBLoginWindowDYLIB.h"
 #import "ZKSwizzle.h"
 #import <objc/runtime.h>
 
@@ -35,45 +34,10 @@ void install() {
             ZKSwizzle(wb_LUI2Window, LUI2Window);
             ZKSwizzle(wb_LUI2MessageViewController, LUI2MessageViewController);
         }
-//        ZKSwizzle(wb_LUI2TextField, LUI2TextField);
-//        ZKSwizzle(wb_NSTextView, NSTextView);
-//        ZKSwizzle(wb_Login1, Login1);
-//        ZKSwizzle(wb_DTDisplay, DTDisplay);
     });
 }
 
-@interface NSObject (logProperties)
-- (void) logProperties;
-@end
-
-@implementation NSObject (logProperties)
-- (void)logProperties {
-    
-    NSLog(@"----------------------------------------------- Properties for object %@", self);
-    
-    @autoreleasepool {
-        unsigned int numberOfProperties = 0;
-        objc_property_t *propertyArray = class_copyPropertyList([self class], &numberOfProperties);
-        for (NSUInteger i = 0; i < numberOfProperties; i++) {
-            @try {
-                objc_property_t property = propertyArray[i];
-                NSString *name = [[NSString alloc] initWithUTF8String:property_getName(property)];
-                NSLog(@"Property %@ Value: %@", name, [self valueForKey:name]);
-            }
-            @catch (NSException *exception) {
-                NSLog(@"%@", exception.reason);
-            }
-            @finally {
-                NSLog(@"Hmmm Fail...");
-            }
-        }
-        free(propertyArray);
-    }
-    NSLog(@"-----------------------------------------------");
-}
-@end
-
-@interface DBLoginWindowDYLIB()
+@interface DBLoginWindowDYLIB : NSObject
 @end
 
 @implementation DBLoginWindowDYLIB
@@ -146,25 +110,24 @@ void install() {
 
 @implementation wb_LUI2MessageViewController
 
-- (void)viewDidAppear {
-    ZKOrig(void);
-    NSTextView *tv = [self valueForKey:@"messageTextView"];
-    
+-  (void)textStorage:(id)arg1 didProcessEditing:(unsigned long long)arg2 range:(struct _NSRange)arg3 changeInLength:(long long)arg4 {
+    NSTextStorage *t = arg1;
     if (db_EnableSize) {
         double lockSize = [db_LockSize doubleValue];
-        if (lockSize < 0.0 || lockSize > 64.0) {
-            // For some reason we need to call this twice?
-            [tv setFont:[NSFont fontWithDescriptor:tv.font.fontDescriptor size:lockSize]];
-            [tv setFont:[NSFont fontWithDescriptor:tv.font.fontDescriptor size:lockSize]];
-        }
+        [t setFont:[NSFont fontWithDescriptor:t.font.fontDescriptor size:lockSize]];
     }
-    
+    ZKOrig(void, t, arg2, arg3, arg4);
+}
+
+- (void)viewDidAppear {
     if (db_EnableText) {
+        NSTextView *tv = [self valueForKey:@"messageTextView"];
         NSString* lockText = db_LockText;
         if ([lockText isEqualToString:@""])
             lockText = @"🍣";
         tv.string = lockText;
     }
+    ZKOrig(void);
 }
 
 @end
@@ -214,265 +177,3 @@ void install() {
 }
 
 @end
-
-// Testing
-
-/*
-
-@interface wb_LUI2TextField : NSTextField
-@end
-
-@implementation wb_LUI2TextField
-
-- (void)setStringValue:(NSString *)stringValue {
-    NSString *result = stringValue;
-    //    if (db_EnableText) {
-    //        NSString* lockText = db_LockText;
-    //        NSLog(@"poop kaydog %@", lockText);
-    //        if ([lockText isEqualToString:@""])
-    //            lockText = @"🍣";
-    //        result = lockText;
-    //    }
-    ZKOrig(void, result);
-}
-
-- (void)setFont:(NSFont *)font {
-    NSFont *result = font;
-    //    if (db_EnableSize) {
-    //        double lockSize = [db_LockSize doubleValue];
-    //        NSLog(@"poop kaydog %f", lockSize);
-    //        if (lockSize < 0.0 || lockSize > 64.0)
-    //            result = [NSFont fontWithDescriptor:[self.font fontDescriptor] size:lockSize * 2.];
-    //    }
-    ZKOrig(void, result);
-}
-
-@end
-
-@interface wb_NSWindowController : NSWindowController
-@end
-
-@implementation wb_NSWindowController
-
-- (void)showWindow:(id)sender {
-    ZKOrig(void, sender);
-    NSLog(@"windowDidLoad %@ : %@", self.className, self.window.className);
-}
-
-- (void)windowWillLoad {
-    ZKOrig(void);
-    NSLog(@"windowDidLoad %@ : %@", self.className, self.window.className);
-}
-
-@end
-
-@interface wb_NSViewController : NSViewController
-@end
-
-@implementation wb_NSViewController
-
-- (void)setMessage:(id)arg1 {
-    NSLog(@"Hookers %@ : %@", [self className], arg1);
-    ZKOrig(void, arg1);
-}
-
-- (void)viewDidAppear {
-    ZKOrig(void);
-    
-    NSLog(@"viewDidAppear %@ : %@", self.className, self.view.className);
-    
-    @try {
-        NSLog(@"Contentview: %@", [self.view valueForKey:@"contentView"]);
-    }
-    @catch (NSException *exception) {
-        NSLog(@"%@", exception.reason);
-    }
-    @finally {
-        NSLog(@"Hmmm Fail...");
-    }
-    
-    if ([[self className] isEqualToString:@"LUI2BackgroundViewController"]) {
-        NSLog(@"Hello sir : %@", [self className]);
-        if (db_EnableAnim) {
-            NSLog(@"We're in sir : %@", [self className]);
-            NSString *picturePath;
-            for (NSString *ext in @[@"jpg", @"png", @"gif"])
-                if ([FileManager fileExistsAtPath:[db_LockFile stringByAppendingPathExtension:ext]])
-                    picturePath = [db_LockFile stringByAppendingPathExtension:ext];
-            
-            NSView *theView = self.view;
-            
-            [self.view logProperties];
-            
-            NSImageView *view = [[NSImageView alloc] initWithFrame:theView.frame];
-            NSImage *theImage = [[NSImage alloc] initWithContentsOfFile:picturePath];
-            [theImage setSize: NSMakeSize(theView.frame.size.width, theView.frame.size.height)];
-            view.image = theImage;
-            view.canDrawSubviewsIntoLayer = YES;
-            [view setAutoresizingMask:NSViewWidthSizable | NSViewHeightSizable];
-            
-            //            Boolean addView = true;
-            //            for (NSView *v in self.view.subviews) {
-            //                if (v.tag == 69) {
-            //                    addView = false;
-            //                    break;
-            //                }
-            //            }
-            //
-            //            if (addView) {
-            //                NSMutableArray *viewz = theView.subviews.mutableCopy;
-            //                [view setTag:69];
-            //                [viewz addObject:view];
-            //                [theView setSubviews:viewz];
-            //            }
-            
-            if (db_LockAnim) {
-                view.imageScaling = NSImageScaleNone;
-                view.animates = YES;
-                //                NSView *layerview = [[NSImageView alloc] initWithFrame:win.contentView.frame];
-                //                layerview.wantsLayer = YES;
-                //                [layerview addSubview:view];
-                //                [layerview setAutoresizingMask:NSViewWidthSizable | NSViewHeightSizable];
-                //                [win.contentView setSubviews:@[layerview]];
-            } else {
-                //                [win.contentView setSubviews:@[view]];
-            }
-            
-            NSLog(@"%@", theView.subviews);
-            
-            //            [theView setSubviews:@[view]];
-        }
-    }
-}
-
-- (void)viewDidLoad {
-    ZKOrig(void);
-    
-    NSLog(@"viewDidLoad Turd wrangler sir : %@", self.view);
-    
-    if ([[self className] isEqualToString:@"LUI2BackgroundViewController"]) {
-        NSLog(@"Hello sir : %@", [self className]);
-        if (db_EnableAnim) {
-            NSLog(@"We're in sir : %@", [self className]);
-            NSString *picturePath;
-            for (NSString *ext in @[@"jpg", @"png", @"gif"])
-                if ([FileManager fileExistsAtPath:[db_LockFile stringByAppendingPathExtension:ext]])
-                    picturePath = [db_LockFile stringByAppendingPathExtension:ext];
-            
-            
-            
-            //            NSWindow *win = [self valueForKey:@"_mainWindow"];
-            //            NSImageView *view = [[NSImageView alloc] initWithFrame:win.contentView.frame];
-            //            NSImage *theImage = [[NSImage alloc] initWithContentsOfFile:picturePath];
-            //            [theImage setSize: NSMakeSize(theView.frame.size.width, theView.frame.size.height)];
-            //            theView.image = theImage;
-            //            theView.canDrawSubviewsIntoLayer = YES;
-            //            [theView setAutoresizingMask:NSViewWidthSizable | NSViewHeightSizable];
-            
-            //        NSButton *adButton = [[NSButton alloc] initWithFrame:NSMakeRect(win.contentView.frame.size.width - 160, 10, 150, 22)];
-            //        [adButton setTitle:@"🍭 Try cDock"];
-            //        [adButton setBezelStyle:NSTexturedRoundedBezelStyle];
-            //        [adButton setTarget:self];
-            //        [adButton setAction:@selector(getcDock)];
-            
-            //            if (db_LockAnim) {
-            //                theView.imageScaling = NSImageScaleNone;
-            //                theView.animates = YES;
-            ////                NSView *layerview = [[NSImageView alloc] initWithFrame:win.contentView.frame];
-            ////                layerview.wantsLayer = YES;
-            ////                [layerview addSubview:view];
-            ////                [layerview setAutoresizingMask:NSViewWidthSizable | NSViewHeightSizable];
-            ////                [win.contentView setSubviews:@[layerview]];
-            //            } else {
-            ////                [win.contentView setSubviews:@[view]];
-            //            }
-        }
-    }
-}
-
-@end
-
-@interface wb_DTDisplay : NSObject
-@end
-
-@implementation wb_DTDisplay
-
-- (void)updateDisplay:(unsigned int)arg1 {
-    NSLog(@"loginwindow Hookers updateDisplay:%ul", arg1);
-    ZKOrig(void, arg1);
-}
-
-- (void)transition {
-    NSLog(@"loginwindow Hookers transition");
-    ZKOrig(void);
-}
-
-- (void)appear {
-    NSLog(@"loginwindow Hookers appear");
-    ZKOrig(void);
-}
-
-- (void)relinquishMain {
-    NSLog(@"loginwindow Hookers relinquish");
-    ZKOrig(void);
-}
-
-- (void)becomeMain {
-    NSLog(@"loginwindow Hookers become");
-    ZKOrig(void);
-}
-
-@end
-
-@interface wb_Login1 : NSObject
-@end
-
-@implementation wb_Login1
-
-- (void)do_autologin_check {
-    NSLog(@"loginwindow Hookers do_autologin_check");
-    ZKOrig(void);
-}
-
-- (BOOL)lockScreenIfNeeded {
-    NSLog(@"loginwindow Hookers lockScreenIfNeeded");
-    return ZKOrig(BOOL);
-}
-
-- (void)doLoginHook {
-    NSLog(@"loginwindow Hookers doLoginHook");
-    ZKOrig(void);
-}
-
-- (BOOL)isDarkInstall {
-    NSLog(@"loginwindow Hookers isDarkInstall");
-    return ZKOrig(BOOL);
-}
-
-@end
-
-@interface wb_NSTextView : NSTextView
-@end
-
-@implementation wb_NSTextView
-
-- (void)setString:(NSString *)stringValue {
-    NSString *result = stringValue;
-    if (db_EnableSize) {
-        double lockSize = [db_LockSize doubleValue];
-        [self setFont:[NSFont fontWithDescriptor:self.font.fontDescriptor size:lockSize]];
-        [self setFont:[NSFont fontWithDescriptor:self.font.fontDescriptor size:lockSize]];
-    }
-    if (db_EnableText) {
-        NSString* lockText = db_LockText;
-        NSLog(@"poop kaydog %@", lockText);
-        if ([lockText isEqualToString:@""])
-            lockText = @"🍣";
-        result = lockText;
-    }
-    ZKOrig(void, result);
-}
-
-@end
-
-*/
